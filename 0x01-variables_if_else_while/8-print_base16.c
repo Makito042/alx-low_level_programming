982ff36f2f7915572b1858f804d434fa02068d87
makuochi <makuochiokoye@gmail.com>
2023-03-20 19:57:26 -0700

    hoes

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - main function
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+int i;
+
+for (i = 0; i < 10; i++)
+{
+putchar(i + '0');
+}
+
+for (i = 0; i < 6; i++)
+{
+putchar(i + 'a');
+}
+
+putchar('\n');
+
+return (0);
+}
+
